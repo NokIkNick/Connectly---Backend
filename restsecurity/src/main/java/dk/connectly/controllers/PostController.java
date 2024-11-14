4ea8eff3534f9199ef5db27192cd357f5b4715a6
mbm1337 <mikkelsen96@live.dk>
2024-11-14 11:23:30 +0100

    PostController

---
+++ restsecurity/src/main/java/dk/connectly/controllers/PostController.java
@@ -0,0 +1,15 @@
+package dk.connectly.controllers;
+
+import java.util.logging.Handler;
+
+public class PostController {
+
+    private PostController() {
+
+    }
+
+    public static Handler createPost() {
+        return null;
+    }
+
+}
