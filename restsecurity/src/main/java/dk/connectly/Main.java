7749ee1b09fa5e4a7cac2ed567771605044eb4bf
Sir Rolin <nicolaieno@msn.com>
2024-11-26 14:56:35 +0100

    scenarioTests

--- restsecurity/src/main/java/dk/connectly/Main.java
+++ restsecurity/src/main/java/dk/connectly/Main.java
@@ -1,5 +1,7 @@
 package dk.connectly;
 
+import java.util.HashMap;
+
 import dk.connectly.config.ApplicationConfig;
 import dk.connectly.config.HibernateConfig;
 import dk.connectly.config.Routes;
@@ -8,8 +10,44 @@ import dk.connectly.utils.Populator;
 import jakarta.persistence.EntityManagerFactory;
 
 public class Main {
+    private static String currentSetting = "";
+    private static String buildString = "";
+    
     public static void main(String[] args) {
-        boolean isTesting = false;
+        HashMap<String, String> settings = new HashMap<>();
+        Runnable addToSettings = () -> {
+            settings.put(currentSetting, buildString);
+            buildString = "";
+            currentSetting = "";
+        };
+
+        for (String arg : args) {
+            if(arg.charAt(0) == '-'){
+                if(currentSetting != ""){
+                    addToSettings.run();
+                }
+                currentSetting = arg;
+            } else if(arg.charAt(0) == '"') {
+                buildString = arg.substring(1);
+            } else if(buildString != ""){
+                if(arg.charAt(arg.length()-1) == '"'){
+                    buildString += arg.substring(0, arg.length() - 1);
+                    addToSettings.run();
+                } else {
+                    buildString += arg;
+                }
+            } else {
+                buildString = arg;
+                addToSettings.run();
+            }
+        }
+
+        // the last setting needs to be added if it doesn't have a corresponding text. example -t for testing
+        if(currentSetting != "" & buildString == "") {
+            addToSettings.run();
+        }
+
+        boolean isTesting = settings.containsKey("t") ? true : false;
         @SuppressWarnings("unused")
         ApplicationConfig app = ApplicationConfig.getInstance()
                 .initiateServer()
--- restsecurity/src/main/java/dk/connectly/dtos/NewConnectionDTO.java
+++ restsecurity/src/main/java/dk/connectly/dtos/NewConnectionDTO.java
@@ -5,6 +5,11 @@ import java.util.Set;
 import dk.connectly.utils.ConnectionType;
 import dk.connectly.utils.ConnectionTypeSetConverter;
 import jakarta.persistence.Convert;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class NewConnectionDTO {
   public UserDTO connection;
